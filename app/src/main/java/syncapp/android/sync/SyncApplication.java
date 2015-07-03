57acf00be30079e9f6bb6164f4a9be7a0df0f516
Jason Jose <jjose1384@gmail.com>
2015-07-03 14:31:57 -0400

    Initial Sync commit

---
+++ app/src/main/java/syncapp/android/sync/SyncApplication.java
@@ -0,0 +1,32 @@
+package syncapp.android.sync;
+
+import android.app.Application;
+
+import com.parse.Parse;
+import com.parse.ParseACL;
+import com.parse.ParseCrashReporting;
+import com.parse.ParseUser;
+
+public class SyncApplication extends Application {
+
+    @Override
+    public void onCreate() {
+        super.onCreate();
+
+        // Initialize Crash Reporting.
+        ParseCrashReporting.enable(this);
+
+        // Enable Local Datastore.
+        Parse.enableLocalDatastore(this);
+
+        // Add your initialization code here
+        Parse.initialize(this, "zC3gzSXrlOaB9OUEcvRPUkTo5X6VWi1dz6kTA50i", "AyS6DdNIwZg9Nve86PnsXwTC5Npc26KzBDOOuQLB");
+
+
+        ParseUser.enableAutomaticUser();
+        ParseACL defaultACL = new ParseACL();
+        // Optionally enable public read access.
+        // defaultACL.setPublicReadAccess(true);
+        ParseACL.setDefaultACL(defaultACL, true);
+    }
+}
