ef930d034aad29c378d175ff03612e4793539ce9
Jason Jose <jjose1384@gmail.com>
2015-07-03 23:42:26 -0400

    test github
    modified the required password length to 5

--- app/src/main/java/syncapp/android/sync/LoginActivity.java
+++ app/src/main/java/syncapp/android/sync/LoginActivity.java
@@ -163,7 +163,7 @@ public class LoginActivity extends Activity implements LoaderCallbacks<Cursor> {
 
     private boolean isPasswordValid(String password) {
         //TODO: Replace this with your own logic
-        return password.length() > 4;
+        return password.length() > 5;
     }
 
     /**
